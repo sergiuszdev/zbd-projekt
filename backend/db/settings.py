22d046c6866f71184459a45ae532b90750e5cc12
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2025-04-08 00:15:11 +0200

    init .gitignore

--- backend/db/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-USER = '' if os.getenv('DB_HOST') is None else os.getenv('DB_HOST')
-PASSWORD = '' if os.getenv('DB_PASSWORD') is None else os.getenv('DB_PASSWORD')
-DB_NAME = '' if os.getenv('DB_NAME') is None else os.getenv('DB_NAME')
