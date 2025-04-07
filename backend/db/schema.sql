8f25823d182850c2a308c151eff0681536afe608
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2025-04-08 00:56:19 +0200

    init

---
+++ backend/db/schema.sql
@@ -0,0 +1,62 @@
+CREATE TABLE "authors" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "bio" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "genres" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar UNIQUE NOT NULL
+);
+
+CREATE TABLE "books" (
+  "id" uuid PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "isbn" varchar(13) UNIQUE NOT NULL,
+  "publish_date" date,
+  "description" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "books_authors" (
+  "book_id" uuid NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+  "author_id" uuid NOT NULL REFERENCES authors(id) ON DELETE CASCADE,
+  PRIMARY KEY ("book_id", "author_id")
+);
+
+CREATE TABLE "books_genres" (
+  "book_id" uuid NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+  "genre_id" uuid NOT NULL REFERENCES genres(id) ON DELETE CASCADE,
+  PRIMARY KEY ("book_id", "genre_id")
+);
+
+CREATE TABLE "users" (
+  "id" uuid PRIMARY KEY,
+  "email" varchar(255) UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
+  "is_employee" boolean NOT NULL DEFAULT false,
+  "first_name" varchar(255) NOT NULL,
+  "last_name" varchar(255) NOT NULL,
+  "phone_number" varchar(20) NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  CHECK (phone_number ~ '^[0-9]+$')
+);
+
+CREATE TABLE "borrowings" (
+  "id" uuid PRIMARY KEY,
+  "user_id" uuid REFERENCES users(id) ON DELETE SET NULL,
+  "book_id" uuid NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+  "borrowed_at" timestamp DEFAULT (now()),
+  "due_at" timestamp NOT NULL,
+  "returned_at" timestamp,
+  CHECK (due_at > borrowed_at),
+  CHECK (returned_at IS NULL OR returned_at >= borrowed_at)
+);
+
+CREATE TABLE "storage" (
+  "id" uuid PRIMARY KEY,
+  "book_id" uuid NOT NULL UNIQUE REFERENCES books(id) ON DELETE CASCADE,
+  "total_copies" int NOT NULL DEFAULT 1 CHECK (total_copies >= 0),
+  "available_copies" int NOT NULL DEFAULT 1 CHECK (available_copies >= 0 AND available_copies <= total_copies)
+);
