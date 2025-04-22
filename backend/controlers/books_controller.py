caf9df95555aa62fbde5f334a44e6b4c9ee903b8
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2025-04-22 18:13:21 +0200

    search by title

--- backend/controlers/books_controller.py
+++ backend/controlers/books_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+import asyncpg
+from fastapi import APIRouter, Depends, Query, HTTPException
 
 from db.db_handler import get_db
 
@@ -9,3 +10,15 @@ router = APIRouter()
 async def get_all(db=Depends(get_db)):
     query = "SELECT * FROM books"
     return await db.fetch(query)
+
+
+@router.get("/search")
+async def search_book(db=Depends(get_db), q: str = Query(min_length=1, description="Fraza wyszukiwana po tytule")):
+
+    query = f"SELECT title FROM books WHERE title ILIKE '%{q}%'"
+
+    results = await db.fetch(query)
+    return {
+        "ilość rekordów": len(results),
+        "wyniki": results
+    }
