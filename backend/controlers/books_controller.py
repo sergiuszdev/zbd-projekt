3944085be9145640c3332914885137e6d1e5122c
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2025-04-23 00:57:26 +0200

    complete search

--- backend/controlers/books_controller.py
+++ backend/controlers/books_controller.py
@@ -13,12 +13,57 @@ async def get_all(db=Depends(get_db)):
 
 
 @router.get("/search")
-async def search_book(db=Depends(get_db), q: str = Query(min_length=1, description="Fraza wyszukiwana po tytule")):
+async def search_book(
+    db=Depends(get_db),
+    q: str = Query(min_length=1, description="Fraza wyszukiwana po tytule"),
+):
+    query = """
+        SELECT books.title, authors.name AS authors_name
+        FROM books
+        JOIN books_authors ON books.id = books_authors.book_id
+        JOIN authors ON authors.id = books_authors.author_id
+        WHERE books.title ILIKE $1 OR authors.name ILIKE $1
+    """
+    params = [f"%{q}%"]
 
-    query = f"SELECT title FROM books WHERE title ILIKE '%{q}%'"
+    results = await db.fetch(query, *params)
+    return {"ilość rekordów": len(results), "wyniki": results}
 
-    results = await db.fetch(query)
-    return {
-        "ilość rekordów": len(results),
-        "wyniki": results
-    }
+
+@router.get("/advanced_search")
+async def search_book(
+    db=Depends(get_db),
+    q: str | None = Query(None, description="Fraza wyszukiwana po tytule lub autorze"),
+    filter: str | None = Query(None, description="Kategoria książki"),
+):
+    query = """
+        SELECT books.title, authors.name AS authors_name 
+        FROM books 
+        JOIN books_authors ON books.id = books_authors.book_id 
+        JOIN authors ON authors.id = books_authors.author_id
+        JOIN books_genres ON books.id = books_genres.book_id 
+        JOIN genres ON books_genres.genre_id = genres.id 
+    """
+
+    conditions = []
+    params = []
+    param_counter = 1
+
+    if q:
+        conditions.append(
+            f"(books.title ILIKE ${param_counter} OR authors.name ILIKE ${param_counter})"
+        )
+        params.append(f"%{q}%")
+        param_counter += 1
+
+    if filter:
+        conditions.append(f"genres.name = ${param_counter}")
+        params.append(filter)
+        param_counter += 1
+
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+
+    results = await db.fetch(query, *params)
+
+    return {"ilość rekordów": len(results), "wyniki": results}
